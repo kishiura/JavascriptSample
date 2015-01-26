418c19f67a58fdd1950ee1d002be64ad4f0e5f13
pipo <psychedelic.nekopunch@gmail.com>
2015-01-26 20:14:06 +0900

    add var

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello:)');
+'use strict';
+
+var testInt     = 0;
+var testString  = 'テスト';
+var testBoolean = true;
+var testArray   = ['配列1', '配列2', '配列3', '配列4'];
+var testObject  = {test: 'hoge'};
+
+
+console.log(testInt);
+console.log(testBoolean);
+console.log(testString);
+console.log(testArray);
+console.log(testObject);
