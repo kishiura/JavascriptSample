74c9e7ead8cfaaaeb096c6776e9acf6de665fdba
pipo <psychedelic.nekopunch@gmail.com>
2015-01-27 13:41:56 +0900

    add

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ var testBoolean = true;
 var testArray   = ['配列1', '配列2', '配列3', '配列4'];
 var testObject  = {test: 'hoge'};
 
-
 console.log(testInt);
 console.log(testBoolean);
 console.log(testString);
 console.log(testArray);
 console.log(testObject);
+
+testInt = 5;
