a753b217552198b90c3f1161158b059953d4f83d
kishiura <kishiura.aoizemi@gmail.com>
2015-02-06 19:24:19 +0900

    書いた事はかいたけど…

--- js/sample.js
+++ js/sample.js
@@ -3,4 +3,10 @@ $(document).ready(function()
 {
 	'use strict';
 	console.log('enjoy javascript ;)');
+	function SetColor (backColor) {
+		if (target != null) {
+		target = document.getElementById("testpage");
+		target.style.backgroundColor = document.changebox.cc.value;
+        }
+	}
 });
